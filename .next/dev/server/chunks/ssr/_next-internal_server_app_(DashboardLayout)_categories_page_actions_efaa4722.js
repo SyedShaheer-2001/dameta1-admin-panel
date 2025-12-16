4037dffe749513bad8c6a944e2fc4083d3eefe8b
SyedShaheer-2001 <shaheer@chameleon-ideas.com>
2025-12-16 23:51:32 +0500

    .next deleted

--- .next/dev/server/chunks/ssr/_next-internal_server_app_(DashboardLayout)_categories_page_actions_efaa4722.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(DashboardLayout)/categories/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28DashboardLayout%29_categories_page_actions_efaa4722.js.map
--- .next/dev/static/chunks/src_app_(DashboardLayout)_categories_page_jsx_69b86490._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@mui_3d80ab8c._.js",
-  "static/chunks/src_app_components_shared_Categories_jsx_a77051a8._.js"
-],
-    source: "dynamic"
-});
